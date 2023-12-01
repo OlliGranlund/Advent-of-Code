ef3d4355466a3e47359392c5357e4b9708e7fb68
Oliver Granlund <oliver@linear.fi>
2023-12-01 06:42:26 +0200

    prepare for 2023

---
+++ 2023/0101.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync('input/01.txt', 'utf8');
+const inputAsArray = input.split(/\n/);
+
+let results = [];
+let targetIndex = 0;
+
+for (let i = 0; i < inputAsArray.length; i++){
+
+  if( inputAsArray[i] === "" ){
+    targetIndex++;
+    results[targetIndex] = 0;
+  } else {
+    if( results.length === 0 ){
+      results[targetIndex] = parseInt( inputAsArray[i] );
+    } else {
+      results[targetIndex] = parseInt(results[targetIndex]) + parseInt(inputAsArray[i]);
+    }
+  }
+}
+
+console.log( '0101: ' + Math.max( ...results ) );
